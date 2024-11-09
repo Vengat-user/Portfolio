f87004d9d135670c67b06db6301ff84213b141b6
vengatesh <vengatbharat@gmail.com>
2024-11-09 21:40:31 +0530

    Initial commit

---
+++ components/Avatar.js
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+const Avatar = () => {
+  return (
+  <div>
+    <Image
+      src={'/avatar.png'}
+      width={737}
+      height={678}
+      alt=""
+      className="translate-z-0 w-full h-full"    
+    />    
+  </div>
+  )
+};
+
+export default Avatar;
